aec94cd74999ec9033e00cb471fc986ddfc15b7d
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-19 12:24:20 -0500

    feat: Implementar servicio API para obtener publicaciones de usuarios

---
+++ app/src/main/java/com/huillca/lab09/PostApiService.kt
@@ -0,0 +1,12 @@
+package com.huillca.lab09
+
+import retrofit2.http.*
+
+interface PostApiService {
+    @GET("posts")
+    suspend fun getUserPosts(): List<PostModel>
+
+    @GET("posts/{id}")
+    suspend fun getUserPostById(@Path("id") id: Int): PostModel
+
+}
